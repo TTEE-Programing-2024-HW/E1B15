1dcda42708df2741a725af3f2b95251fc7ab9d63
E1B15 <u11202215@o365.ttu.edu.tw>
2024-06-01 23:03:38 +0800

    設置密碼，建立表格

---
+++ hw4.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void) {
+    int i,password;
+    struct student{
+    	char name[10];
+		int id[20];
+    	int math[10];
+    	int ph[10];
+    	int en[10];
+    	
+	};
+    /*printf("   /\\_/\\  \n");
+    printf("  / o o \\ \n");
+    printf(" (   \"   )\n");
+    printf("  \\~(*)~/ \n");
+    printf("   \\~ ~/  \n");
+    printf("    | |   \n");
+    printf("   /___\\  \n");
+    for( i=1;i<=3;i++){
+	printf("½Ð¿é¤J±K½X\n");
+	scanf("%d",&password);
+	if(password==2024){
+		printf("±K½X¥¿½T");
+		break;
+	}
+	printf("±K½X¿ù»~%d¦¸\n",i);
+	if(i==3){
+		return 0;
+	}
+	
+}
+system("cls");*/
+ while(1){
+	printf("--------[grade System]--------------\n");
+	printf("| a. Enter student grades       |\n");
+	printf("| b. Display student grades     |\n");
+	printf("| c. Search for student grades  |\n");
+	printf("| d. Grade ranking              |\n");
+	printf("| e. Exit system                |\n");
+	printf("------------------------------------\n");
+}
+}
